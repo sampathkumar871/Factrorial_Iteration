f8be0aee2a3c9818581f52ff7069766d31821e5d
Sampat Kumar <Sampathkumar871@gmail.com>
2018-07-30 15:19:51 +0530

    //factorial_itteration
    //factorial_itteration

---
+++ Factrorial_Iteration/Factrorial_Iteration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factrorial_Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+            Console.WriteLine("Enter the Number to find the factorial in itteration method");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = number; i > 0; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
